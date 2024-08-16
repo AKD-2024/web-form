b3b43bfc736c66730404c09449171798c5ad93f2
Anjali <anjali.dubey@comprotechnologies.com>
2024-08-16 17:34:13 +0530

    Vue: Training.

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,133 @@
+<template>
+  <form>
+    <label>Email:</label>
+    <input type="email" required v-model="email" />
+
+    <label>Password:</label>
+    <input type="password" required v-model="password" />
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+      <option value="tester">Web Tester</option>
+    </select>
+
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="skills">
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
+
+    <!-- Checkbox  Way - 1: 2-way Data Binding -->
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required />
+      <label>Accept terms and conditions</label>
+    </div>
+
+    <!-- Checkbox  Way - 2: 2-way Data Binding -->
+    <!-- <div>
+      <input type="checkbox" value="anjali" v-model="names">
+      <label>Anjali</label>
+    </div>
+    <div>
+      <input type="checkbox" value="kumari" v-model="names">
+      <label>Kumari</label>
+    </div>
+    <div>
+      <input type="checkbox" value="dubey" v-model="names">
+      <label>Dubey</label>
+    </div> -->
+  </form>
+  <p></p>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Terms Checked: {{ terms }}</p>
+  <!-- <p>Names Checked: {{ names }}</p> -->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      role: "developer",
+      terms: false,
+      tempSkill: "",
+      skills: [],
+      // names: [],
+    };
+  },
+  methods: {
+    addSkill(e) {
+      if (
+        e.key === "Enter" &&
+        this.tempSkill &&
+        !this.skills.includes(this.tempSkill)
+      ) {
+        this.skills.push(this.tempSkill);
+        this.tempSkill = "";
+      }
+    },
+    deleteSkill(skill) {
+      this.skills = this.skills.filter((item) => {
+        return skill !== item;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+
+input,
+select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+.skills {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
+}
+</style>
